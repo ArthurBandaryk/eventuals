bae40d6236c2c530f893524e1473727342d67367
Zakhar Pilipuk <zpilipuk@gmail.com>
2022-02-15 13:55:58 +0300

    wip

--- eventuals/catch.h
+++ eventuals/catch.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <any>
 #include <memory> // For 'std::unique_ptr'.
 #include <optional>
 #include <tuple>
+#include <variant> // For 'std::monostate'.
 
 #include "eventuals/eventual.h"
 #include "eventuals/then.h"
@@ -17,36 +19,106 @@ struct _Catch {
   template <typename Error_, typename Handler_>
   struct Handler {
     using error_type = Error_;
+    Handler_ handler_;
+  };
 
-    template <typename E>
-    static constexpr bool Handles =
-        std::is_same_v<Error_, E> || std::is_base_of_v<Error_, E>;
+  template <bool Eventual_, typename HandlerResult_, typename K_>
+  struct ContinuationIf {
+    using continuation = std::monostate;
+  };
+
+  template <typename HandlerResult_, typename K_>
+  struct ContinuationIf<true, HandlerResult_, K_> {
+    using continuation =
+        decltype(std::declval<HandlerResult_>().template k<void>(
+            std::declval<_Then::Adaptor<K_>>()));
+  };
+
+  template <typename K_, typename Handler_, typename Error_>
+  struct HandlerWithK {
+    using error_type = Error_;
+
+    HandlerWithK(Handler_&& handler)
+      : handler_(std::move(handler)) {}
+
+    template <typename K, typename... Args>
+    void BuildAndInvoke(K& k, Interrupt* interrupt, Args&&... args) {
+      using HandlerInvokeResult =
+          typename std::invoke_result<Handler_, Args...>::type;
+
+      if constexpr (HasValueFrom<HandlerInvokeResult>::value) {
+        constructed_k_.emplace(
+            (handler_(std::forward<Args>(args)...)
+                 .template k<void>(_Then::Adaptor<K>{k})));
+
+        if (interrupt != nullptr) {
+          constructed_k_->Register(*interrupt);
+        }
+
+        constructed_k_->Start();
+      } else if constexpr (std::is_void_v<HandlerInvokeResult>) {
+        handler_(std::forward<Args>(args)...);
+        k.Fail();
+      } else {
+        k.Start(handler_(std::forward<Args>(args)...));
+      }
+    }
+
+    template <typename K, typename E>
+    bool Handle(K& k, Interrupt* interrupt, E&& error) {
+      if constexpr (
+          std::is_same_v<Error_, E> || std::is_base_of_v<Error_, E>) {
+        BuildAndInvoke(k, interrupt, std::move(error));
+        return true;
+      } else if constexpr (std::is_same_v<E, std::exception_ptr>) {
+        try {
+          std::rethrow_exception(error);
+        } catch (Error_ e) {
+          BuildAndInvoke(k, interrupt, std::move(e));
+          return true;
+        } catch (...) {
+          // if constexpr (std::is_same_v<Error_, AllHandler>) {
+          //   BuildAndInvoke(k, interrupt, std::current_exception());
+          //   return true;
+          // } else {
+          //   return false;
+          // }
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    // Only 'all' hander can be executed with no errors.
+    template <typename K>
+    bool Handle(K& k, Interrupt* interrupt) {
+      BuildAndInvoke(k, interrupt);
+      return true;
+    }
 
     Handler_ handler_;
+
+    using E_ = typename std::conditional_t<
+        std::is_void_v<Error_>,
+        std::invoke_result<Handler_>,
+        std::invoke_result<Handler_, Error_>>::type;
+
+    using ConstructedK_ =
+        typename ContinuationIf<HasValueFrom<E_>::value, E_, K_>::continuation;
+
+    std::optional<ConstructedK_> constructed_k_;
   };
 
   ////////////////////////////////////////////////////////////////////////
 
-  template <typename K_, bool has_all_, typename... CatchHandlers_>
+  template <
+      typename K_,
+      typename AllHandler_,
+      typename CatchHandlersWithKTuple_>
   struct Continuation {
-    template <typename F, typename... Args>
-    void BuildAndInvoke(F&& f, Args&&... args) {
-      using HandlerInvokeResult =
-          typename std::invoke_result<F, Args...>::type;
-
-      if constexpr (HasValueFrom<HandlerInvokeResult>::value) {
-        auto k = (f(std::forward<Args>(args)...)
-                      .template k<void>(_Then::Adaptor<K_>{k_}));
-
-        if (interrupt_ != nullptr) {
-          k.Register(*interrupt_);
-        }
-
-        k.Start();
-      } else {
-        k_.Start(f(std::forward<Args>(args)...));
-      }
-    }
+    static constexpr bool HasAll =
+        !std::is_same_v<AllHandler_, std::monostate>;
 
     template <
         typename Error_,
@@ -56,12 +128,13 @@ struct _Catch {
         Error_&& error,
         CatchHandler_&& handler,
         Tail&&... tail) {
-      if constexpr (CatchHandler_::template Handles<Error_>) {
-        BuildAndInvoke(std::move(handler.handler_), std::move(error));
-      } else {
-        UnpackInvokeHelper<Error_>(
-            std::move(error),
-            std::forward<Tail>(tail)...);
+      if (!processed_) {
+        processed_ = handler.Handle(k_, interrupt_, std::move(error));
+        if (!processed_) {
+          UnpackInvokeHelper<Error_>(
+              std::move(error),
+              std::forward<Tail>(tail)...);
+        }
       }
     }
 
@@ -69,18 +142,8 @@ struct _Catch {
     void UnpackInvokeHelper(
         Error_&& error,
         CatchHandler_&& handler) {
-      if constexpr (CatchHandler_::template Handles<Error_>) {
-        BuildAndInvoke(std::move(handler.handler_), std::move(error));
-      } else if constexpr (has_all_) {
-        // Last handler should be always 'all' handler.
-        BuildAndInvoke(
-            std::move(
-                std::get<
-                    std::tuple_size<
-                        decltype(catch_handlers_)>::value
-                    - 1>(catch_handlers_)
-                    .handler_),
-            std::move(error));
+      if (!processed_) {
+        processed_ = handler.Handle(k_, interrupt_, std::move(error));
       }
     }
 
@@ -91,12 +154,23 @@ struct _Catch {
 
     template <typename... Args>
     void Fail(Args&&... args) {
-      // propagate k.Fail otherwise?
       static_assert(
           sizeof...(args) == 0 || sizeof...(args) == 1,
           "Catch only supports 0 or 1 argument, but found > 1");
 
-      if constexpr (sizeof...(args) > 0) {
+      if constexpr (HasAll) {
+        if constexpr (sizeof...(args) == 1) {
+          all_handler_ = std::make_any<HandlerWithK<K_, AllHandler_, Args...>>(
+              // 'AllHandler_' there is just a
+              // lambda function for 'all' handler.
+              std::move(std::any_cast<AllHandler_>(std::move(all_handler_))));
+        } else {
+          all_handler_ = std::make_any<HandlerWithK<K_, AllHandler_, void>>(
+              std::any_cast<AllHandler_>(std::move(all_handler_)));
+        }
+      }
+
+      if constexpr (sizeof...(args) == 1) {
         std::apply(
             [&](auto&&... catch_handlers) {
               UnpackInvokeHelper(
@@ -104,18 +178,37 @@ struct _Catch {
                   std::forward<decltype(catch_handlers)>(catch_handlers)...);
             },
             std::move(catch_handlers_));
+
+        if constexpr (HasAll) {
+          if (!processed_) {
+            processed_ = std::any_cast<
+                             HandlerWithK<
+                                 K_,
+                                 AllHandler_,
+                                 Args...>>(std::move(all_handler_))
+                             .Handle(
+                                 k_,
+                                 interrupt_,
+                                 std::forward<decltype(args)>(args)...);
+          }
+        }
       } else {
-        // Last handler should be always 'all' handler.
-        if constexpr (has_all_) {
-          BuildAndInvoke(std::move(
-              std::get<
-                  std::tuple_size<
-                      decltype(catch_handlers_)>::value
-                  - 1>(catch_handlers_)
-                  .handler_));
+        // Only 'all' hander can be executed with no errors.
+        if constexpr (HasAll) {
+          processed_ = std::any_cast<
+                           HandlerWithK<
+                               K_,
+                               AllHandler_,
+                               void>>(std::move(all_handler_))
+                           .Handle(k_, interrupt_);
         } else {
           k_.Fail(std::forward<decltype(args)>(args)...);
         }
+      }
+
+      // Should be executed if no handlers executed before.
+      if (!processed_) {
+        k_.Fail(std::forward<decltype(args)>(args)...);
       }
     }
 
@@ -129,31 +222,85 @@ struct _Catch {
     }
 
     K_ k_;
-    std::tuple<CatchHandlers_...> catch_handlers_;
+    CatchHandlersWithKTuple_ catch_handlers_;
+    std::any all_handler_;
+    bool processed_ = false;
 
     Interrupt* interrupt_ = nullptr;
   };
 
-  template <bool has_all_ = false, typename... CatchHandlers_>
+  template <
+      // Can't use 'void' instead because of storing in 'std::optional'.
+      typename AllHandler_ = std::monostate,
+      typename... CatchHandlers_>
   struct Builder {
     template <typename Arg>
     using ValueFrom = Arg;
 
+    static constexpr bool HasAll =
+        !std::is_same_v<AllHandler_, std::monostate>;
+
+    template <size_t i, typename K, typename Tuple>
+    auto AddTypeToTuple(Tuple&& tuple) {
+      if constexpr (i < std::tuple_size_v<decltype(catch_handlers_)>) {
+        auto current = std::get<i>(catch_handlers_);
+        auto add_curent = std::apply(
+            [&](auto&&... exist) {
+              return std::make_tuple(
+                  std::move(exist)...,
+                  HandlerWithK<
+                      K,
+                      decltype(current.handler_),
+                      typename decltype(current)::error_type>{
+                      std::move(current.handler_)});
+            },
+            tuple);
+
+        return AddTypeToTuple<i + 1, K>(std::move(add_curent));
+      } else
+        return std::move(tuple);
+    }
+
+    template <size_t i, typename K>
+    auto AddTypeToTuple() {
+      static_assert(
+          i < std::tuple_size_v<decltype(catch_handlers_)>,
+          "No catch handlers specified");
+
+      auto current = std::get<i>(catch_handlers_);
+
+      return AddTypeToTuple<i + 1, K>(
+          std::make_tuple(
+              HandlerWithK<
+                  K,
+                  decltype(current.handler_),
+                  typename decltype(current)::error_type>{
+                  std::move(current.handler_)}));
+    }
+
     template <typename Arg, typename K>
     auto k(K k) && {
       static_assert(
-          sizeof...(CatchHandlers_) > 0 || has_all_,
+          sizeof...(CatchHandlers_) > 0 || HasAll,
           "No handlers were specified for 'Catch'");
 
-      return Continuation<K, has_all_, CatchHandlers_...>{
+      // We want to store 'K' at 'CatchHandler' because if the
+      // handler returns an 'Eventual', we need to provide the next
+      // 'k' for it.
+      // 'AddTypeToTuple' just creates new tuple with 'CatchHandlers',
+      // but also provides 'K' type for each.
+      auto handlers_with_k = AddTypeToTuple<0, K>();
+
+      return Continuation<K, AllHandler_, decltype(handlers_with_k)>{
           std::move(k),
-          std::move(catch_handlers_)};
+          std::move(handlers_with_k),
+          std::make_any<AllHandler_>(std::move(all_handler_))};
     }
 
     template <typename E, typename F>
     auto raised(F f) {
       static_assert(
-          !has_all_,
+          !HasAll,
           "Handler for all errors should be installed last");
 
       // Push back new handler 'F' for error type 'E' to
@@ -161,37 +308,28 @@ struct _Catch {
       return std::apply(
           [&](auto&&... catch_handler) {
             return Builder<
-                has_all_,
+                std::monostate,
                 CatchHandlers_...,
                 Handler<E, F>>{
                 std::make_tuple(
                     std::move(catch_handler)...,
-                    Handler<E, F>{std::move(f)})};
+                    Handler<E, F>{std::move(f)}),
+                std::move(all_handler_)};
           },
           std::move(catch_handlers_));
     }
 
     template <typename F>
     auto all(F f) {
-      static_assert(!has_all_, "Duplicate 'all'");
+      static_assert(!HasAll, "Duplicate 'all'");
 
-      // Push back new handler 'F' for all error types (using 'void')
-      // to already stored 'CatchHandlers'.
-      return std::apply(
-          [&](auto&&... callback) {
-            return Builder<
-                true,
-                CatchHandlers_...,
-                Handler<void, F>>{
-                std::make_tuple(
-                    std::move(callback)...,
-                    Handler<void, F>{
-                        std::move(f)})};
-          },
-          std::move(catch_handlers_));
+      return Builder<
+          F,
+          CatchHandlers_...>{std::move(catch_handlers_), std::move(f)};
     }
 
     std::tuple<CatchHandlers_...> catch_handlers_;
+    AllHandler_ all_handler_;
   };
 };
 
--- test/catch.cc
+++ test/catch.cc
@@ -74,6 +74,186 @@ TEST(CatchTest, ChildException) {
   EXPECT_EQ(10, *e());
 }
 
+TEST(CatchTest, AllValue) {
+  auto e = []() {
+    return Just(1)
+        | Raise(1)
+        | Catch()
+              .raised<double>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'double'";
+              })
+              .raised<std::exception>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'std::exception'";
+              })
+              .all([](auto&&...) {
+                return 10;
+              });
+  };
+
+  EXPECT_EQ(10, *e());
+}
+
+// TEST(CatchTest, AllEventual) {
+//   auto e = []() {
+//     return Just(1)
+//         | Raise(1)
+//         | Catch()
+//               .raised<double>([](auto&& error) {
+//                 FAIL() << "Encountered an unexpected raised 'int'";
+//               })
+//               .raised<std::exception>([](auto&& error) {
+//                 FAIL() << "Encountered an unexpected raised 'int'";
+//               })
+//               .all([](auto&&...) {
+//                 return Just(10);
+//               });
+//   };
+
+//   EXPECT_EQ(10, *e());
+// }
+
+TEST(CatchTest, NoExactHandler) {
+  auto e = []() {
+    return Just(1)
+        | Raise(1)
+        | Catch()
+              .raised<double>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'double'";
+              })
+              .raised<std::exception>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'std::exception'";
+              });
+  };
+
+  EXPECT_THROW(*e(), int);
+}
+
+TEST(CatchTest, AllWithArgument) {
+  auto checker = [](int error) {
+    EXPECT_EQ(error, 1);
+  };
+
+  auto e = [&]() {
+    return Just(1)
+        | Raise(1)
+        | Catch()
+              .raised<double>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'double'";
+              })
+              .raised<std::exception>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'std::exception'";
+              })
+              .all([&](auto&&... error) {
+                checker(error...);
+                return 10;
+              });
+  };
+
+  EXPECT_EQ(*e(), 10);
+}
+
+TEST(CatchTest, ExceptionPtrAllValue) {
+  auto expected = []() -> Expected::Of<int> {
+    return Unexpected(10);
+  };
+
+  auto checker = [](auto error) {
+    try {
+      std::rethrow_exception(error);
+    } catch (int x) {
+      EXPECT_EQ(x, 10);
+    } catch (...) {
+      FAIL() << "Failure on rethrowing";
+    }
+  };
+
+  auto e = [&]() {
+    return expected()
+        | Catch()
+              .raised<double>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'double'";
+              })
+              .raised<std::exception>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'std::exception'";
+              })
+              // Receive 'std::exception_ptr' there.
+              .all([&](auto&&... error) {
+                checker(error...);
+                return 10;
+              });
+  };
+
+  EXPECT_EQ(*e(), 10);
+}
+
+TEST(CatchTest, ExceptionPtrAllEmpty) {
+  auto expected = []() -> Expected::Of<int> {
+    return Unexpected(10);
+  };
+
+  auto checker = [](auto error) {
+    try {
+      std::rethrow_exception(error);
+    } catch (int x) {
+      EXPECT_EQ(x, 10);
+    } catch (...) {
+      FAIL() << "Failure on rethrowing";
+    }
+  };
+
+  auto e = [&]() {
+    return expected()
+        | Catch()
+              .raised<double>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'double'";
+              })
+              .raised<std::exception>([](auto&& error) {
+                FAIL() << "Encountered an unexpected raised 'std::exception'";
+              })
+              // Receive 'std::exception_ptr' there.
+              .all([&](auto&&... error) {
+                checker(error...);
+              });
+  };
+
+  EXPECT_THROW(*e(), std::runtime_error);
+}
+
+// TEST(CatchTest, ExceptionPtrAllEventual) {
+//   auto expected = []() -> Expected::Of<int> {
+//     return Unexpected(10);
+//   };
+
+//   auto checker = [](auto error) {
+//     try {
+//       std::rethrow_exception(error);
+//     } catch (int x) {
+//       EXPECT_EQ(x, 10);
+//     } catch (...) {
+//       FAIL() << "Failure on rethrowing";
+//     }
+//   };
+
+//   auto e = [&]() {
+//     return expected()
+//         | Catch()
+//               .raised<double>([](auto&& error) {
+//                 FAIL() << "Encountered an unexpected raised 'double'";
+//               })
+//               .raised<std::exception>([](auto&& error) {
+//                 FAIL() <<
+//"Encountered an unexpected raised 'std::exception'";
+//               })
+//               // Receive 'std::exception_ptr' there.
+//               .all([&](auto&&... error) {
+//                 checker(error...);
+//                 return Just(10);
+//               });
+//   };
+
+//   EXPECT_EQ(10, *e());
+// }
+
 TEST(CatchTest, Expected) {
   struct Error : public std::exception {
     const char* what() const noexcept override {
@@ -91,8 +271,8 @@ TEST(CatchTest, Expected) {
               .raised<int>([](auto&& error) {
                 FAIL() << "Encountered an unexpected raised 'int'";
               })
-              .raised<std::exception_ptr>([](auto&& error) {
-                //EXPECT_EQ("child exception", error.what());
+              .raised<Error>([](auto&& error) {
+                EXPECT_EQ(std::string("child exception"), error.what());
                 return Just(10);
               });
   };
@@ -109,7 +289,7 @@ TEST(CatchTest, ReRaise) {
                 return Raise(1);
               })
               .raised<std::exception_ptr>([](auto&& error) {})
-              .all([](auto&& error) {})
+              .all([](auto&&... error) {})
         | Catch()
               .raised<int>([](auto&& error) {
                 EXPECT_EQ(error, 1);
@@ -134,8 +314,8 @@ TEST(CatchTest, NoError) {
               .raised<std::exception>([](auto&& error) {
                 FAIL() << "Encountered an unexpected raised 'std::exception'";
               })
-              .all([]() {
-                return Just(10);
+              .all([](auto&&...) {
+                return 10;
               });
   };
 
